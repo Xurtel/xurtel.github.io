91e16578504cbfa0414f366ed665e827a7ee3cc7
Matthew <mngor@ucsc.edu>
2019-07-27 18:50:44 -0700

    Profile image to Intro.jsx, update Intro

--- src/components/Hello.jsx
+++ src/components/Hello.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import './Hello.css';
 
+import myPicture from '../images/my-picture.png';
+
 class Hello extends React.Component{
     render(){
         return(
             <div className = "container">
+
+                <img src = {myPicture} alt = "me"/>
                 <header>
                 <h1>
                     Hello!
@@ -13,10 +17,18 @@ class Hello extends React.Component{
                 </header>
                 <p>
                     My name is Matthew! I am currently a junior studying Computer Science at the University of California, Santa Cruz. 
-                    I love learning, creating solutions to problems, and making an impact. 
-                    ~ I will write more here ~
+                    I got interested in software engineering and web development because of how much 
+                        positive change and great convenience software and technology brings into our lives. 
 
+                    My passions in life include learning, creating solutions to problems, and making an impact. 
+                    I love seeing how much technology is evolving over the years, and the 
+                        never-ending challenge to keep up with it inspires me to keep pushing forward along with it.
+
+                    When I'm not at the desk, you can bet that I'll be playing a team-oriented sport like basketball and volleyball,
+                        hiking, travelling, or simply hanging out with friends and family&nbsp;
+                    <span role = "img" aria-label = "cheerful">😁</span>
                 </p>
+                <p id = "quote">“If you want to go fast, go alone. If you want to go far, go together.” </p>
             </div>
             
         );
--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
-import myPicture from '../images/my-picture.png';
+
 
 class Intro extends React.Component{
     render(){
@@ -8,14 +8,12 @@ class Intro extends React.Component{
             <div className = "container">
 
                 <h1 id = "intro">Matthew Ngor</h1>
-                <p>Computer Science Undergraduate</p>
-                <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. 
-                    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
-                    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
-                <Link to ="/resume"> Route to resume </Link> 
+                <p id = "occupation">Computer Science Undergraduate</p>
                 
-                <img src = {myPicture} alt = "me"/>
+                <Link to ="/resume"> Route to resume </Link> 
+                <Link to ="/linkedin"> Route to LinkedIn </Link> 
+                <Link to ="/github"> Route to Github </Link> 
+                
 
             </div>
         );
