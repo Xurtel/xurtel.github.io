442a4c517ba06331152e0fdf4c24583b7c315ebc
Matthew <mngor@ucsc.edu>
2020-09-22 21:03:25 -0700

    new uco logo for Experience.jsx

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -4,7 +4,7 @@ import './Experience.css';
 
 import zohoLogo from '../images/zoho-logo.png';
 import slusdLogo from '../images/slusd-logo.png';
-import ucoLogo from '../images/uco-logo.v2.png';
+import ucoLogo from '../images/uco-logo.jpg';
 
 class Experience extends React.Component{
     render(){
