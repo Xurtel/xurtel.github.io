69675c47f1c1ee8b5a2d4fe3db07aa8104d33444
Matthew <mngor@ucsc.edu>
2021-07-15 21:00:47 -0700

    Updates

---
+++ precache-manifest.32b707996bbd616c29b246e4997ffebb.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df59abdf2b7977fd20bc545fe660b903",
+    "url": "/xurtel.github.io/index.html"
+  },
+  {
+    "revision": "eb49621b3828b6a35356",
+    "url": "/xurtel.github.io/static/css/main.be8a6094.chunk.css"
+  },
+  {
+    "revision": "407a143c6a019a51e761",
+    "url": "/xurtel.github.io/static/js/2.52f4494c.chunk.js"
+  },
+  {
+    "revision": "eb49621b3828b6a35356",
+    "url": "/xurtel.github.io/static/js/main.8a389e8f.chunk.js"
+  },
+  {
+    "revision": "f32afa4faa442fd44fb7",
+    "url": "/xurtel.github.io/static/js/runtime~main.6158d5b7.js"
+  },
+  {
+    "revision": "fcc354d9a232b803bf89b3af7081edd7",
+    "url": "/xurtel.github.io/static/media/bagito-bag.fcc354d9.png"
+  },
+  {
+    "revision": "dd70d975781389b0035485904279a9a0",
+    "url": "/xurtel.github.io/static/media/bidirectional-data-sync.dd70d975.jpg"
+  },
+  {
+    "revision": "ac9aebb1eeb0fb5c0cddce9351c7fa8f",
+    "url": "/xurtel.github.io/static/media/city-at-night.ac9aebb1.jpg"
+  },
+  {
+    "revision": "0f7f36b7cdfd4fdc21484b6cb3ac2365",
+    "url": "/xurtel.github.io/static/media/csa-logo.0f7f36b7.png"
+  },
+  {
+    "revision": "e8cfa9ef789309a117c24c68d4427064",
+    "url": "/xurtel.github.io/static/media/keyClub-logo.e8cfa9ef.png"
+  },
+  {
+    "revision": "9ae714042790bf19a864335cd9d65925",
+    "url": "/xurtel.github.io/static/media/my-picture.9ae71404.png"
+  },
+  {
+    "revision": "a4136f623254edc8129ebf3921a7104d",
+    "url": "/xurtel.github.io/static/media/resistors.a4136f62.jpg"
+  },
+  {
+    "revision": "51c2c6e15faa2aa549499ccc88cc5812",
+    "url": "/xurtel.github.io/static/media/resume.51c2c6e1.pdf"
+  },
+  {
+    "revision": "5872a0241c8b0fd604dc1eda53200199",
+    "url": "/xurtel.github.io/static/media/slusd-logo.5872a024.png"
+  },
+  {
+    "revision": "6540610a378d106eb6eb4a3ce2d09bdd",
+    "url": "/xurtel.github.io/static/media/uco-logo.v1.6540610a.png"
+  },
+  {
+    "revision": "3850e3ffe44e520a9b1bc709165226be",
+    "url": "/xurtel.github.io/static/media/ucsc-logo.3850e3ff.png"
+  },
+  {
+    "revision": "455839b7acbaf7aaea02e34d2ad71ea2",
+    "url": "/xurtel.github.io/static/media/zoho-logo.455839b7.png"
+  }
+]);
